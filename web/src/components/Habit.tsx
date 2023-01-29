3eeb346f5b540631442aa3c7937493f68758815e
Marcelo A Macagnan <marcelo.a.mac@gmail.com>
2023-01-29 17:17:14 -0300

    20230129

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 interface HabitProps {
   completed: number;
 }
 
-export function Habit() {
-  return <h1>Habit </h1>;
+export function Habit(props: HabitProps) {
+  return <p className="bg-zinc-900 w-20 h-10 text-white rounded m-2 flex items-center justify-center">teste {props.completed}</p>;
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html'
-  ],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
